b10c2428f726d3206fd5810f5e0fa5489caea7b5
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-04-28 23:38:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :price
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    
+  end
+
+  def add_item(item, price, quantity = 0)
+    @price = price
+    @total += price * quantity
+    items_added = 0
+    while items_added < quantity
+      @items << name
+      items_added += 1
+  end
+end
+  def apply_discount
+    if @discount == 0 then "No discount applies"
+    else
+      @total =(@total*(1-@discount.to.f/100))
+      "With discount, the total comes to $#{total.round}"
+  end
+  
+  def void_last_transaction
+    @total = @total - @price
+  end
+  def items
+    @items = items
+  end
+end
